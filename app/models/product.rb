c4c65ea9722da5b01f90b1ca31b8641d90c47aa0
Sean Holden <seanholden1@gmail.com>
2012-07-31 00:28:41 +0100

    completed catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,5 @@ class Product < ActiveRecord::Base
   	           with: %r{\.(gif|jpg|png)$}i, 
   	           message: 'must be a URL for GIF, JPG or PNG image.'
                }
-
+  validates_length_of :title, :minimum => 10, :message => "Needs to be more than 10 characters in length"
 end
