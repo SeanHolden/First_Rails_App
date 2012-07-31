825d26d136c24a406cab84a2af8bd7f410bac27f
Sean Holden <seanholden1@gmail.com>
2012-07-31 12:22:02 +0100

    Created the cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,12 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
   	@time = Time.now
+  	increment_session_counter
+  end
+
+  def increment_session_counter
+  	session[:counter] ||= 0
+    session[:counter] += 1
+    @sessioncounter = session[:counter]
   end
 end
