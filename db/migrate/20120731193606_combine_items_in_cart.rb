539e0f896aaca53509f06ac6cc9139cf7a841db3
Sean Holden <Sean.Holden@o2.com>
2012-08-01 10:23:17 +0100

    added a redirect for invalid cart, some stylesheets and a total price for all items in cart

--- db/migrate/20120731193606_combine_items_in_cart.rb
+++ db/migrate/20120731193606_combine_items_in_cart.rb
@@ -6,7 +6,7 @@ class CombineItemsInCart < ActiveRecord::Migration
       sums = cart.line_items.group(:product_id).sum(:quantity)
       
       sums.each do |product_id, quantity| 
-    	if quantity > 1
+        if quantity > 1
           # remove individual items
           cart.line_items.where(product_id: product_id).delete_all
           # replace with a single item
@@ -17,5 +17,15 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def down
+    # split items with quantity>1 into multiple items 
+    LineItem.where("quantity>1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id,
+          product_id: line_item.product_id, quantity: 1
+       end
+      # remove original item
+      line_item.destroy
+    end 
   end
 end
